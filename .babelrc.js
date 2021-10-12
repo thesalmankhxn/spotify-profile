c14611fd91f868891312ce8b7b4f237f4dd095f1
iamSK77 <iamSK77.x@gmail.com>
2021-10-12 16:49:46 +0530

    addons: add styled-components package

--- .babelrc.js
+++ .babelrc.js
@@ -10,5 +10,9 @@ module.exports = {
       },
     ],
   ],
-  plugins: ["@emotion/babel-plugin", "babel-plugin-macros"],
+  plugins: [
+    "@emotion/babel-plugin",
+    "babel-plugin-macros",
+    ["styled-components", { ssr: true }],
+  ],
 };
--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,50 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { extractCritical } from "@emotion/server";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
     const initialProps = await Document.getInitialProps(ctx);
-    const page = await ctx.renderPage();
+    const page = ctx.renderPage;
     const styles = extractCritical(page.html);
-    return { ...initialProps, ...page, ...styles };
+
+    try {
+      ctx.renderPage = () =>
+        page({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <style
-            data-emotion-css={this.props.ids.join(" ")}
-            dangerouslySetInnerHTML={{ __html: this.props.css }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+  //   render() {
+  //     return (
+  //       <Html lang="en">
+  //         <Head>
+  //           <style
+  //             data-emotion-css={this.props.ids.join(" ")}
+  //             dangerouslySetInnerHTML={{ __html: this.props.css }}
+  //           />
+  //         </Head>
+  //         <body>
+  //           <Main />
+  //           <NextScript />
+  //         </body>
+  //       </Html>
+  //     );
+  //   }
 }
