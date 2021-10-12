36e7d53448345259dbf1616ee73f30be9d5e9d8f
iamSK77 <iamSK77.x@gmail.com>
2021-10-12 16:50:46 +0530

    fix: remove unused code

--- pages/_document.js
+++ pages/_document.js
@@ -30,21 +30,4 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
-
-  //   render() {
-  //     return (
-  //       <Html lang="en">
-  //         <Head>
-  //           <style
-  //             data-emotion-css={this.props.ids.join(" ")}
-  //             dangerouslySetInnerHTML={{ __html: this.props.css }}
-  //           />
-  //         </Head>
-  //         <body>
-  //           <Main />
-  //           <NextScript />
-  //         </body>
-  //       </Html>
-  //     );
-  //   }
 }
